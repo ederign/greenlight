0934d6770dcf2d456457591832922990c685e6ad
Eder Ignatowicz <ignatowicz@gmail.com>
2024-04-28 11:05:44 -0400

    chapter 4

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type envelope map[string]any
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 
@@ -19,4 +22,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+
+	js, err := json.MarshalIndent(data, "", "\t")
+
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
